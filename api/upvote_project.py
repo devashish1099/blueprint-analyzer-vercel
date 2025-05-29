dff849becef1cf6969b418a9434884c28416fd01
devashish <devashish1099@gmail.com>
2025-05-29 12:59:10 +0000

    extra changes

---
+++ api/upvote_project.py
@@ -0,0 +1,34 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+project_upvotes_count = 0
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', 'https://blueprint-upload-ui.vercel.app')
+        self.end_headers()
+        response_data = {'count': project_upvotes_count}
+        self.wfile.write(json.dumps(response_data).encode('utf-8'))
+        return
+
+    def do_POST(self):
+        global project_upvotes_count
+        project_upvotes_count += 1
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', 'https://blueprint-upload-ui.vercel.app')
+        self.end_headers()
+        response_data = {'newCount': project_upvotes_count}
+        self.wfile.write(json.dumps(response_data).encode('utf-8'))
+        return
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', 'https://blueprint-upload-ui.vercel.app')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
