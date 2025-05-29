ee4d6242c4937b8a702d1c205dd53226e1b64b48
devashish <devashish1099@gmail.com>
2025-05-29 13:10:37 +0000

    fixing CORs

--- api/upvote_meme.py
+++ api/upvote_meme.py
@@ -4,7 +4,7 @@ import json
 meme_upvotes_count = 0
 
 class handler(BaseHTTPRequestHandler):
-
+    
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
@@ -26,9 +26,9 @@ class handler(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(response_data).encode('utf-8'))
         return
 
-    # def do_OPTIONS(self):
-    #     self.send_response(200)
-    #     self.send_header('Access-Control-Allow-Origin', 'https://blueprint-upload-ui.vercel.app')
-    #     self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    #     self.send_header('Access-Control-Allow-Headers', 'Content-Type')
-    #     self.end_headers()
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', 'https://blueprint-upload-ui.vercel.app')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
